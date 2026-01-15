4ee24c6a5ce31f7ab9c99554a0394bcf9a72dfb6
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2026-01-15 21:19:19 +0530

    U[dates

---
+++ server-2/index.js
@@ -0,0 +1,49 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+let todos = [];
+let counter = 1;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Server 2");
+});
+// get all todos
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+// get single todo by id
+app.get("/todos/:todoId", (req, res) => {
+  const todoId = req.params.todoId;
+  const foundTodo = todos.filter((todo) => todo.id == todoId);
+  res.send(foundTodo);
+});
+
+// create todo
+app.post("/todos", (req, res) => {
+  const { todo } = req.body;
+  todos.push({ todo, id: counter });
+  counter++;
+  res.status(201).send("Todo created successfully.");
+});
+
+// update todo
+app.put("/todos/:todoId", (req, res) => {
+
+
+
+  res.send("PUT request to update todo");
+});
+
+// delete todo
+app.delete("/todos/:todoId", (req, res) => {
+
+    
+  res.send("DELETE request to delete todo");
+});
+
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
+});
